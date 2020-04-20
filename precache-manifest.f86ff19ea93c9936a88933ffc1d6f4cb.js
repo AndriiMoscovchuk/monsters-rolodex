c16b74d641db77b210853a76ae8509357c24b3b1
Andrii Moskovchuk <moskovchuka@ryanair.com>
2020-04-20 20:21:52 +0300

    Updates

---
+++ precache-manifest.f86ff19ea93c9936a88933ffc1d6f4cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da5839278b0d3528bd8201598c72619",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1c330882bdeb1dd04819",
+    "url": "/monsters-rolodex/static/css/main.538e94d4.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c330882bdeb1dd04819",
+    "url": "/monsters-rolodex/static/js/main.7823a26c.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
